0b72a17d37f1f25e7dbed8dc5e76041f6ce59343
JounQin <admin@1stg.me>
2019-08-19 06:10:30 +0800

    feat: format css related files via stylelint automatically

--- packages/lint-staged/index.js
+++ packages/lint-staged/index.js
@@ -3,8 +3,5 @@ module.exports = {
   '*.{css,less}': ['stylelint --config @1stg/stylelint-config'],
   '*.{sass,scss,vue}': ['stylelint --config @1stg/stylelint-config/scss'],
   '.*rc': ['prettier --write', 'git add'],
-  '*.{css,gql,html,json,less,md,sass,scss,vue,yml}': [
-    'prettier --write',
-    'git add',
-  ],
+  '*.{gql,html,json,md,vue,yml}': ['prettier --write', 'git add'],
 }
