63f933231828c2888121345d525bda9ddd576982
JounQin <admin@1stg.me>
2019-09-12 23:13:14 +0800

    feat(eslint): enable `excludedFiles` feature to simplify ts linter

--- packages/eslint-config/overrides.js
+++ packages/eslint-config/overrides.js
@@ -43,119 +43,135 @@ try {
     : require.resolve('@1stg/tsconfig')
 } catch (e) {}
 
-exports.ts = {
-  files: '*.{ts,tsx}',
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project,
-  },
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/typescript',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: ['@typescript-eslint'],
-  settings: {
-    'import/resolver': {
-      ts: {
-        alwaysTryTypes: true,
-      },
-    },
-    node: {
-      resolvePaths: [path.resolve('node_modules/@types')],
-      tryExtensions: [
-        '.ts',
-        '.tsx',
-        '.d.ts',
-        '.vue',
-        '.js',
-        '.jsx',
-        '.json',
-        '.node',
-      ],
-    },
-  },
-  rules: {
-    '@typescript-eslint/adjacent-overload-signatures': 2,
-    '@typescript-eslint/array-type': [
-      2,
-      {
-        default: 'array-simple',
-      },
+exports.ts = [
+  {
+    files: '*.{ts,tsx}',
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/typescript',
+      'prettier/@typescript-eslint',
     ],
-    '@typescript-eslint/consistent-type-definitions': [2, 'interface'],
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/explicit-member-accessibility': [
-      2,
-      {
-        accessibility: 'no-public',
-        overrides: {
-          parameterProperties: 'off',
+    plugins: ['@typescript-eslint'],
+    settings: {
+      'import/resolver': {
+        ts: {
+          alwaysTryTypes: true,
         },
       },
-    ],
-    '@typescript-eslint/member-naming': [
-      2,
-      {
-        private: '^_',
+      node: {
+        resolvePaths: [path.resolve('node_modules/@types')],
+        tryExtensions: [
+          '.ts',
+          '.tsx',
+          '.d.ts',
+          '.vue',
+          '.js',
+          '.jsx',
+          '.json',
+          '.node',
+        ],
       },
-    ],
-    '@typescript-eslint/member-ordering': 2,
-    '@typescript-eslint/no-empty-function': 2,
-    '@typescript-eslint/no-extraneous-class': 2,
-    '@typescript-eslint/no-floating-promises': 2,
-    '@typescript-eslint/no-for-in-array': 2,
-    '@typescript-eslint/no-misused-promises': 2,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-parameter-properties': 0,
-    '@typescript-eslint/no-require-imports': 2,
-    '@typescript-eslint/no-this-alias': [
-      2,
-      {
-        allowDestructuring: true,
-        allowedNames: ['self'],
-      },
-    ],
-    '@typescript-eslint/no-type-alias': [
-      2,
-      {
-        allowAliases: 'in-unions-and-intersections',
-        allowCallbacks: 'always',
-        allowLiterals: 'in-unions-and-intersections',
-        allowMappedTypes: 'always',
-      },
-    ],
-    '@typescript-eslint/no-unnecessary-qualifier': 2,
-    '@typescript-eslint/no-unnecessary-type-arguments': 2,
-    '@typescript-eslint/no-useless-constructor': 2,
-    '@typescript-eslint/no-unused-vars': [
-      2,
-      {
-        argsIgnorePattern: '^_',
-      },
-    ],
-    '@typescript-eslint/prefer-for-of': 2,
-    '@typescript-eslint/prefer-function-type': 2,
-    '@typescript-eslint/prefer-readonly': 2,
-    '@typescript-eslint/restrict-plus-operands': 2,
-    '@typescript-eslint/triple-slash-reference': [
-      2,
-      { types: 'prefer-import' },
-    ],
-    '@typescript-eslint/unbound-method': 2,
-    '@typescript-eslint/unified-signatures': 2,
-    'import/default': 0,
-    'import/named': 0,
-    'import/no-duplicates': 2,
-    'import/no-named-as-default': 0,
-    'import/no-named-as-default-member': 0,
-    'no-empty-function': 0,
-    'no-useless-constructor': 0,
-    'node/no-unsupported-features/es-syntax': 0,
-    // @typescript-eslint/no-floating-promises has already handled this case
-    'promise/catch-or-return': 0,
+    },
+    rules: {
+      '@typescript-eslint/adjacent-overload-signatures': 2,
+      '@typescript-eslint/array-type': [
+        2,
+        {
+          default: 'array-simple',
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': [2, 'interface'],
+      '@typescript-eslint/explicit-function-return-type': 0,
+      '@typescript-eslint/explicit-member-accessibility': [
+        2,
+        {
+          accessibility: 'no-public',
+          overrides: {
+            parameterProperties: 'off',
+          },
+        },
+      ],
+      '@typescript-eslint/member-naming': [
+        2,
+        {
+          private: '^_',
+        },
+      ],
+      '@typescript-eslint/member-ordering': 2,
+      '@typescript-eslint/no-empty-function': 2,
+      '@typescript-eslint/no-extraneous-class': 2,
+      '@typescript-eslint/no-for-in-array': 2,
+      '@typescript-eslint/no-non-null-assertion': 0,
+      '@typescript-eslint/no-parameter-properties': 0,
+      '@typescript-eslint/no-require-imports': 2,
+      '@typescript-eslint/no-this-alias': [
+        2,
+        {
+          allowDestructuring: true,
+          allowedNames: ['self'],
+        },
+      ],
+      '@typescript-eslint/no-type-alias': [
+        2,
+        {
+          allowAliases: 'in-unions-and-intersections',
+          allowCallbacks: 'always',
+          allowLiterals: 'in-unions-and-intersections',
+          allowMappedTypes: 'always',
+        },
+      ],
+      '@typescript-eslint/no-useless-constructor': 2,
+      '@typescript-eslint/no-unused-vars': [
+        2,
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/prefer-for-of': 2,
+      '@typescript-eslint/prefer-function-type': 2,
+      '@typescript-eslint/triple-slash-reference': [
+        2,
+        { types: 'prefer-import' },
+      ],
+      '@typescript-eslint/unified-signatures': 2,
+      'import/default': 0,
+      'import/named': 0,
+      'import/no-duplicates': 2,
+      'import/no-named-as-default': 0,
+      'import/no-named-as-default-member': 0,
+      'no-empty-function': 0,
+      'no-useless-constructor': 0,
+      'node/no-unsupported-features/es-syntax': 0,
+      // @typescript-eslint/no-floating-promises has already handled this case
+      'promise/catch-or-return': 0,
+    },
+  },
+  {
+    files: '*.{ts,tsx}',
+    excludedFiles: '*.d.ts',
+    parserOptions: {
+      project,
+    },
+    extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
+    rules: {
+      '@typescript-eslint/no-floating-promises': 2,
+      '@typescript-eslint/no-misused-promises': 2,
+      '@typescript-eslint/no-unnecessary-qualifier': 2,
+      '@typescript-eslint/no-unnecessary-type-arguments': 2,
+      '@typescript-eslint/prefer-readonly': 2,
+      '@typescript-eslint/restrict-plus-operands': 2,
+      '@typescript-eslint/unbound-method': 2,
+    },
+  },
+]
+
+exports.dTs = {
+  files: '*.d.ts',
+  rules: {
+    'import/no-duplicates': 0,
+    'import/order': 0,
   },
 }
 
@@ -171,6 +187,7 @@ try {
 
 exports.tslint = {
   files: '*.{ts,tsx}',
+  excludedFiles: '*.d.ts',
   plugins: ['@typescript-eslint/tslint'],
   rules: {
     '@typescript-eslint/tslint/config': [
@@ -217,32 +234,6 @@ exports.reactTs = {
   },
 }
 
-exports.dTs = {
-  files: '*.d.ts',
-  parserOptions: {
-    project: null,
-  },
-  rules: {
-    '@typescript-eslint/adjacent-overload-signatures': 0,
-    '@typescript-eslint/await-thenable': 0,
-    '@typescript-eslint/no-floating-promises': 0,
-    '@typescript-eslint/no-misused-promises': 0,
-    '@typescript-eslint/no-unnecessary-qualifier': 0,
-    '@typescript-eslint/no-unnecessary-type-arguments': 0,
-    '@typescript-eslint/no-unnecessary-type-assertion': 0,
-    '@typescript-eslint/prefer-regexp-exec': 0,
-    '@typescript-eslint/prefer-includes': 0,
-    '@typescript-eslint/prefer-readonly': 0,
-    '@typescript-eslint/prefer-string-starts-ends-with': 0,
-    '@typescript-eslint/require-await': 0,
-    '@typescript-eslint/restrict-plus-operands': 0,
-    '@typescript-eslint/unbound-method': 0,
-    '@typescript-eslint/tslint/config': 0,
-    'import/no-duplicates': 0,
-    'import/order': 0,
-  },
-}
-
 exports.vue = {
   files: ['*.vue'],
   parser: 'vue-eslint-parser',
@@ -250,7 +241,7 @@ exports.vue = {
     parser: '@typescript-eslint/parser',
     extraFileExtensions: ['.vue'],
   },
-  settings: exports.ts.settings,
+  settings: exports.ts[0].settings,
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
@@ -302,12 +293,12 @@ try {
 
 exports.overrides = [
   exports.js,
-  exports.ts,
+  ...exports.ts,
+  exports.dTs,
   lintFile && tslint && exports.tslint,
   exports.react,
   exports.reactHooks,
   exports.reactTs,
-  exports.dTs,
   exports.vue,
   exports.md,
   exports.mdx,
