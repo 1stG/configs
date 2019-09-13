f10c8dd182246ce1f70a8b5b0a5501da65113bf8
JounQin <admin@1stg.me>
2019-09-13 19:23:40 +0800

    fix(eslint): readdirSync breaks if packages does not exist

--- packages/eslint-config/_util.js
+++ packages/eslint-config/_util.js
@@ -1,16 +1,17 @@
 const fs = require('fs')
 const path = require('path')
 
+exports.identity = function(_) {
+  return _
+}
+
 exports.isMonorepo =
   fs.existsSync(path.resolve('lerna.json')) ||
   !!require(path.resolve('package.json')).workspaces
 
 exports.allowModules = exports.isMonorepo
-  ? fs
+  ? fs.statSync('packages').isDirectory() &&
+    fs
       .readdirSync('packages')
       .map(pkg => require(path.resolve(`packages/${pkg}/package.json`)).name)
   : undefined
-
-exports.identity = function(_) {
-  return _
-}
