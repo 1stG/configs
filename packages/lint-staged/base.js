851afd5d4aa2af30f309c81505b46129255f83e5
JounQin <admin@1stg.me>
2019-09-18 23:57:14 +0800

    feat(lint-staged): add imagemin support

--- packages/lint-staged/base.js
+++ packages/lint-staged/base.js
@@ -1,6 +1,12 @@
+const { resolve } = require('path')
+
 module.exports = {
+  '.*rc': ['prettier --write', 'git add'],
   '*.{js,jsx,md,mdx,mjs,vue}': ['eslint --fix', 'git add'],
   '*.{css,less,sass,scss,vue}': ['stylelint --fix', 'git add'],
-  '.*rc': ['prettier --write', 'git add'],
   '*.{gql,html,json,md,mdx,vue,yaml,yml}': ['prettier --write', 'git add'],
+  '*.{gif,jpeg,jpg,png,svg,webp}': [
+    `node ${resolve(__dirname, 'imagemin')}`,
+    'git add',
+  ],
 }
