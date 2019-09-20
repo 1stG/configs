2ce6de3cd4d3589d24336280985227b25905c8a8
JounQin <admin@1stg.me>
2019-09-20 17:37:43 +0800

    fix(lint-staged): only enable stylelint if it is available

--- packages/lint-staged/base.js
+++ packages/lint-staged/base.js
@@ -1,12 +1,24 @@
 const { resolve } = require('path')
 
-module.exports = {
+let isStylelintAvailable
+
+try {
+  // eslint-disable-next-line node/no-extraneous-require
+  isStylelintAvailable = !!require.resolve('stylelint')
+} catch (e) {}
+
+const config = {
   '.*rc': ['prettier --write', 'git add'],
   '*.{js,jsx,md,mdx,mjs,vue}': ['eslint --fix', 'git add'],
-  '*.{css,less,sass,scss,vue}': ['stylelint --fix', 'git add'],
   '*.{gql,html,json,md,mdx,pug,vue,yaml,yml}': ['prettier --write', 'git add'],
   '*.{gif,jpeg,jpg,png,svg,webp}': [
     `node ${resolve(__dirname, 'imagemin')}`,
     'git add',
   ],
 }
+
+if (isStylelintAvailable) {
+  config['*.{css,less,sass,scss,vue}'] = ['stylelint --fix', 'git add']
+}
+
+module.exports = config
