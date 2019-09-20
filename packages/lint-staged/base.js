7a613eeef50d736d5b22c7643a44b0ea4b3289ac
JounQin <admin@1stg.me>
2019-09-20 20:19:40 +0800

    fix(lint-staged): prepare to extract imagemin in case not all projects need it

--- packages/lint-staged/base.js
+++ packages/lint-staged/base.js
@@ -1,5 +1,3 @@
-const { resolve } = require('path')
-
 let isStylelintAvailable
 
 try {
@@ -7,18 +5,25 @@ try {
   isStylelintAvailable = !!require.resolve('stylelint')
 } catch (e) {}
 
+let isImageminAvailable
+
+try {
+  // eslint-disable-next-line node/no-extraneous-require, node/no-missing-require
+  isImageminAvailable = !!require.resolve('@1stg/imagemin')
+} catch (e) {}
+
 const config = {
   '.*rc': ['prettier --write', 'git add'],
   '*.{js,jsx,md,mdx,mjs,vue}': ['eslint --fix', 'git add'],
   '*.{gql,html,json,md,mdx,pug,vue,yaml,yml}': ['prettier --write', 'git add'],
-  '*.{gif,jpeg,jpg,png,svg,webp}': [
-    `node ${resolve(__dirname, 'imagemin')}`,
-    'git add',
-  ],
 }
 
 if (isStylelintAvailable) {
   config['*.{css,less,sass,scss,vue}'] = ['stylelint --fix', 'git add']
+}
+
+if (isImageminAvailable) {
+  config['*.{gif,jpeg,jpg,png,svg,webp}'] = ['i', 'git add']
 }
 
 module.exports = config
