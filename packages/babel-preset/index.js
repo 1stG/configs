2854921c56fc91434ab7064f6431f934b6ad43db
JounQin <admin@1stg.me>
2019-10-01 10:37:11 +0800

    fix(babel-preset): add babel-plugin-import support with builtin antd support

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -1,106 +1,135 @@
 const { declare } = require('@babel/helper-plugin-utils')
 
-module.exports = declare((api, opts) => {
-  api.assertVersion(7)
+const DEFAULT_ANTD_OPTIONS = {
+  libraryName: 'antd',
+  style: true,
+}
 
-  const {
-    generator,
-    modules = false,
-    react,
-    typescript,
-    vue,
-    useBuiltIns = 'usage',
-  } = opts
-
-  const proposalTypeScriptPreset = require('babel-preset-proposal-typescript')
-
-  const presets = [
-    [
-      proposalTypeScriptPreset,
-      {
-        isTSX: vue,
-      },
-    ],
-    [
-      require('@babel/preset-env'),
-      {
-        modules,
-        exclude: generator
-          ? undefined
-          : [
-              '@babel/transform-async-to-generator',
-              '@babel/transform-regenerator',
-            ],
-        corejs: {
-          version: 3,
-          proposals: true,
-        },
-        useBuiltIns,
-      },
-    ],
-  ]
-
-  const plugins = [require('@babel/plugin-proposal-class-properties')]
-
-  if (!generator) {
-    plugins.push([
-      'module:fast-async',
-      {
-        useRuntimeModule: true,
-      },
-    ])
-  }
-
-  if (typescript) {
-    presets.push(require('@babel/preset-typescript'))
-  }
-
-  const reactPreset = [
-    require('@babel/preset-react'),
+module.exports = declare(
+  (
+    api,
     {
-      development: api.env('development'),
+      generator,
+      import: importOptions,
+      modules = false,
+      react,
+      typescript,
+      vue,
+      useBuiltIns = 'usage',
     },
-  ]
-  const reactPlugin = api.env('production')
-    ? [
-        require('babel-plugin-transform-react-remove-prop-types'),
+  ) => {
+    api.assertVersion(7)
+
+    const proposalTypeScriptPreset = require('babel-preset-proposal-typescript')
+
+    const presets = [
+      [
+        proposalTypeScriptPreset,
         {
-          removeImport: true,
+          isTSX: vue,
         },
-      ]
-    : undefined
-
-  if (react) {
-    presets.push(reactPreset)
-
-    if (reactPlugin) {
-      plugins.push(reactPlugin)
-    }
-  }
-
-  if (vue) {
-    presets.push('@vue/babel-preset-jsx')
-  }
-
-  return {
-    plugins,
-    presets,
-    overrides: vue
-      ? undefined
-      : [
-          {
-            test: /\.(js|md|ts)x$/,
-            plugins: reactPlugin && [reactPlugin],
-            presets: [
-              [
-                proposalTypeScriptPreset,
-                {
-                  isTSX: true, // enable jsx plugin for mdx
-                },
+      ],
+      [
+        require('@babel/preset-env'),
+        {
+          modules,
+          exclude: generator
+            ? undefined
+            : [
+                '@babel/transform-async-to-generator',
+                '@babel/transform-regenerator',
               ],
-              reactPreset,
-            ],
+          corejs: {
+            version: 3,
+            proposals: true,
           },
-        ],
-  }
-})
+          useBuiltIns,
+        },
+      ],
+    ]
+
+    const plugins = [require('@babel/plugin-proposal-class-properties')]
+
+    if (!generator) {
+      plugins.push([
+        require('fast-async'),
+        {
+          useRuntimeModule: true,
+        },
+      ])
+    }
+
+    if (importOptions) {
+      const importPlugin = require('babel-plugin-import')
+      if (importOptions === true) {
+        plugins.push([importPlugin, DEFAULT_ANTD_OPTIONS])
+      } else if (Array.isArray(importOptions)) {
+        plugins.push([importPlugin, DEFAULT_ANTD_OPTIONS].concat(importOptions))
+      } else {
+        plugins.push(
+          [
+            importPlugin,
+            {
+              ...DEFAULT_ANTD_OPTIONS,
+              ...importOptions.antd,
+            },
+          ],
+          ...(importOptions.plugins || []),
+        )
+      }
+    }
+
+    if (typescript) {
+      presets.push(require('@babel/preset-typescript'))
+    }
+
+    const reactPreset = [
+      require('@babel/preset-react'),
+      {
+        development: api.env('development'),
+      },
+    ]
+    const reactPlugin = api.env('production')
+      ? [
+          require('babel-plugin-transform-react-remove-prop-types'),
+          {
+            removeImport: true,
+          },
+        ]
+      : undefined
+
+    if (react) {
+      presets.push(reactPreset)
+
+      if (reactPlugin) {
+        plugins.push(reactPlugin)
+      }
+    }
+
+    if (vue) {
+      presets.push('@vue/babel-preset-jsx')
+    }
+
+    return {
+      plugins,
+      presets,
+      overrides: vue
+        ? undefined
+        : [
+            {
+              test: /\.(js|md|ts)x$/,
+              plugins: reactPlugin && [reactPlugin],
+              presets: [
+                [
+                  proposalTypeScriptPreset,
+                  {
+                    isTSX: true, // enable jsx plugin for mdx
+                  },
+                ],
+                reactPreset,
+              ],
+            },
+          ],
+    }
+  },
+)
