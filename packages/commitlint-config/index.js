4393955fd0beb9bfb7c3f32a5e5f842fbd222a61
JounQin <admin@1stg.me>
2019-10-17 20:21:26 +0800

    fix(commitlint-config): enable lerna-scopes with conventional, close #7

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -2,10 +2,26 @@ let isLernaAvailable
 
 try {
   // eslint-disable-next-line node/no-extraneous-require
-  require.resolve('lerna')
-  isLernaAvailable = true
+  isLernaAvailable = !!require.resolve('lerna')
 } catch (e) {}
 
+const conventionalConfig = require('@commitlint/config-conventional')
+const lernaScopesConfig = require('@commitlint/config-lerna-scopes')
+
+// hard-coded for now
+// TODO: check value types? but is that really necessary?
+const MERGE_PROPERTIES = ['rules', 'utils']
+
 module.exports = isLernaAvailable
-  ? require('@commitlint/config-lerna-scopes')
-  : require('@commitlint/config-conventional')
+  ? Object.assign(
+      conventionalConfig,
+      MERGE_PROPERTIES.reduce((config, property) => {
+        config[property] = Object.assign(
+          {},
+          conventionalConfig[property],
+          lernaScopesConfig[property],
+        )
+        return config
+      }, {}),
+    )
+  : conventionalConfig
