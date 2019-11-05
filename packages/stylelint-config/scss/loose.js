a825a76ebe604f0e4bb67904bcf70eff9825b035
JounQin <admin@1stg.me>
2019-11-05 14:33:42 +0800

    fix(stylelint-config): disable dollar-variable-default and media-feature-value-dollar-variable for loose

--- packages/stylelint-config/scss/loose.js
+++ packages/stylelint-config/scss/loose.js
@@ -3,6 +3,8 @@ const baseConfig = require('.')
 module.exports = Object.assign({}, baseConfig, {
   rules: Object.assign({}, baseConfig.rules, {
     'scss/declaration-nested-properties': null,
+    'scss/dollar-variable-default': null,
+    'scss/media-feature-value-dollar-variable': null,
     'scss/selector-nest-combinators': null,
   }),
 })
