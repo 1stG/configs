00423baade2485950a9eef19790ea0b29ee2c9c9
JounQin <admin@1stg.me>
2021-02-22 20:23:26 +0800

    ci: typecov issue

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -6,11 +6,16 @@ describe('babel-plugin: fast-async', () => {
       transform('async function main() {}', {
         filename: 'fast-async.test.js',
         presets: ['@1stg'],
-      })!.code,
-    ).toMatch(`function main() {
-  return new Promise(function ($return, $error) {
-    return $return();
-  });
-}`)
+      })?.code,
+    ).toMatchInlineSnapshot(`
+      "import \\"core-js/modules/es.object.to-string.js\\";
+      import \\"core-js/modules/es.promise.js\\";
+
+      function main() {
+        return new Promise(function ($return, $error) {
+          return $return();
+        });
+      }"
+    `)
   })
 })
