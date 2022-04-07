3dd80b4645ae084147fe8750b7a20669fa977213
JounQin <admin@1stg.me>
2022-04-08 00:20:20 +0800

    feat(remark): update plugins

--- packages/remark-config/index.js
+++ packages/remark-config/index.js
@@ -1,4 +1,7 @@
+import remarkGfm from 'remark-gfm'
 import remarkLint from 'remark-lint'
+import remarkLintNoDuplicateHeadings from 'remark-lint-no-duplicate-headings'
+import remarkLintNoDuplicateHeadingsInSection from 'remark-lint-no-duplicate-headings-in-section'
 import remarkPresetLintConsistent from 'remark-preset-lint-consistent'
 import remarkPresetLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide'
 import remarkPresetLintRecommended from 'remark-preset-lint-recommended'
@@ -16,6 +19,9 @@ export default {
     remarkPresetLintMarkdownStyleGuide,
     remarkPresetLintRecommended,
     remarkPresetPrettier,
+    remarkGfm,
     remarkValidateLinks,
+    [remarkLintNoDuplicateHeadings, false],
+    remarkLintNoDuplicateHeadingsInSection,
   ],
 }
