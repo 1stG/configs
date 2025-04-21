b8bc5fac446202ac0f7f9b85b3bf4fa12c97d08d
JounQin <admin@1stg.me>
2025-04-21 19:35:08 +0800

    fix(prettier-config): should use `declare` in `.d.ts` (#353)

---
+++ .changeset/great-paws-boil.md
@@ -0,0 +1,5 @@
+---
+"@1stg/prettier-config": patch
+---
+
+fix: should use `declare` in `.d.ts`
--- packages/prettier-config/base.d.ts
+++ packages/prettier-config/base.d.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'prettier'
 
-const config: Config
+declare const config: Config
 
 export default config
